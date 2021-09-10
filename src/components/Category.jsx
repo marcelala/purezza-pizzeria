e54d9bf408a1989cdcf804598ae6f345fa977a4e
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-09-10 09:53:20 +0200

    refactor rendering of product list

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
-
-import List from "./List";
-import ProductCard from "../pages/category/ProductCard";
-
 export default function Category({ item }) {
-	const { id, categoryTitle, subtitle, description, img, products } = item;
+	const { id, categoryTitle, subtitle, description, img } = item;
 	const imageSrc = require(`../assets/img/categories/${img}`).default;
-	const path = useLocation().pathname;
 	return (
-		<article className="category-card">
-			<button key={id}>
-				<img src={imageSrc} alt="delicious food" />
-				<div className="text-box">
-					<h2 className="category-subtitle">{subtitle}</h2>
-					<h1 className="category-name">{categoryTitle}</h1>
-					<p className="category-description">{description}</p>
-				</div>
-				{path !== "/" && <List list={products} Component={ProductCard} />}
-			</button>
-		</article>
+		<li>
+			<article className="category-card">
+				<button key={id}>
+					<img src={imageSrc} alt="delicious food" />
+					<div className="text-box">
+						<h2 className="category-subtitle">{subtitle}</h2>
+						<h1 className="category-name">{categoryTitle}</h1>
+						<p className="category-description">{description}</p>
+					</div>
+				</button>
+			</article>
+		</li>
 	);
 }
--- src/pages/category/ProductCard.jsx
+++ src/pages/category/ProductCard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { useHistory,useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
-export default function ProductCard({ item}) {
-	const {id, name, shortDescription, img } = item;
+export default function ProductCard({ item }) {
+	const { id, name, shortDescription, img } = item;
 	const imageSrc = require(`../../assets/img/products/${img}`).default;
 	const history = useHistory();
 	const category = useLocation().pathname.toLowerCase();
@@ -10,11 +10,13 @@ export default function ProductCard({ item}) {
 	return (
 		<li>
 			<article>
-				<div onClick={() => history.push(url)}>
+				<button className="product-card" onClick={() => history.push(url)}>
 					<img className="product-img" src={imageSrc} alt="delicious food" />
-					<h2 className="product-name">{name}</h2>
-					<p className="category-short-description">{shortDescription}</p>
-				</div>
+					<div className="text-box">
+						<h2 className="product-name">{name}</h2>
+						<p className="category-short-description">{shortDescription}</p>
+					</div>
+				</button>
 			</article>
 		</li>
 	);
