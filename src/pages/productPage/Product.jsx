68198aec2ae3184adec1f707cab1bd49b9050473
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-09-17 12:40:18 +0200

    home alternated cards

--- src/pages/productPage/Product.jsx
+++ src/pages/productPage/Product.jsx
@@ -10,20 +10,22 @@ export default function Product({ product }) {
 	return (
 		<section className="product">
 			<img className="product-img" src={imageSrc} alt="delicious food" />
-			<div className="text-box">
-				<div className="title">
-					<h2 className="oleo">The</h2>
-					<h1 className="exo">{name}</h1>
+			<section className="product-info">
+				<div className="text-box">
+					<div className="title">
+						<h2 className="oleo">The</h2>
+						<h1 className="exo">{name}</h1>
+					</div>
+					<p>{fullDescription}</p>
 				</div>
-				<p>{fullDescription}</p>
-			</div>
-			<div className="ingredients">
-				<h2 className="oleo">Ingredient list</h2>
-				<List list={ingredientList} Component={Ingredient} />
-			</div>
-			<section className="nutrition-facts">
-				<h2 className="oleo">Nutritional Information</h2>
-				<NutritionFacts nutritionalInfo={nutritionalInfo} />
+				<div className="ingredients">
+					<h2 className="oleo">Ingredient list</h2>
+					<List list={ingredientList} Component={Ingredient} />
+				</div>
+				<section className="nutrition-facts">
+					<h2 className="oleo">Nutritional Information</h2>
+					<NutritionFacts nutritionalInfo={nutritionalInfo} />
+				</section>
 			</section>
 		</section>
 	);
